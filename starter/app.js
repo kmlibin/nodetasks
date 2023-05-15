f7402abf2e7e5ec670facfb8ba57c075a5d680de
Kelli Libin <km.putnam2@gmail.com>
2023-05-15 11:05:34 -0600

    added mongoose, .env file, connected to mongodb

--- starter/app.js
+++ starter/app.js
@@ -2,21 +2,39 @@
 const express = require("express");
 const app = express();
 
+//connect to db and .env variable
+const connectDB = require("./db/connect");
+require('dotenv').config
+
 //import routes
-const tasks = require('./routes/tasks')
+const tasks = require("./routes/tasks");
 //need to access json data sent from thefrontend. middleware! get access to req.body
 //middleware
-app.use(express.json())
+app.use(express.json());
 
-//routes. 
+//routes.
 //think about the functionality of your page. editing? posting? deleting?
-///api/v1/tasks is a convention, signals that all of them are the api routes. v1 just allows you to update as you keep working
+///api/v1/tasks is a convention, signals that all of them are the api routes. v1 just allows you to update as you keep working.
+//*be consistent with routes for your users!
+//important b/c we are creating a server, so we want to create an https interface. this allows frontend to interact with our data.
+//REST API representational state transfer.
+//our users can complete CRUD operations
 
-app.get('/hello', (req, res) => {
-    res.send('task manager app')
-})
+app.get("/hello", (req, res) => {
+  res.send("task manager app");
+});
 
-app.use('/api/v1/tasks', tasks)
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-app.listen(port, console.log(`server is listening on ${port}...`));
+
+//start up database, spins up server if connection is successful
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on ${port}...`));
+  } catch {
+    console.log(error);
+  }
+};
+start();
---
+++ starter/db/connect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//1st arg, connection string, 2nd arg is options...these options removed all the deprecation warnings
+const connectDB = (url) => {
+  if (url) {
+    mongoose.connect(url, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+  }
+};
+
+//refactor because right now, the app starts, then database starts. we want it the other way around, that way if
+//we don't connect to DB, the app doesn't load. so, now we invoke mongoose connect in app.js...set it up as a function so
+//we can just invoke it.
+
+module.exports = connectDB;
