33f14e4e3539dbcec06193dd8c419dc60a4d3185
Kelli Libin <km.putnam2@gmail.com>
2023-05-15 10:33:26 -0600

    refactored db connect, invoked in app.js

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -3,7 +3,18 @@ const connectionString =
   "mongodb+srv://KelliAL:Aguq9u0TY5HuSUvJ@nodeexpressproject.ff2yxap.mongodb.net/NodeExpressProject?retryWrites=true&w=majority";
 
 //1st arg, connection string, 2nd arg is options...these options removed all the deprecation warnings
-mongoose
-  .connect(connectionString, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true})
-  .then(() => console.log("connected to database..."))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+    mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+}
+   
+  //refactor because right now, the app starts, then database starts. we want it the other way around, that way if
+  //we don't connect to DB, the app doesn't load. so, now we invoke mongoose connect in app.js...set it up as a function so
+  //we can just invoke it.
+
+  module.exports = connectDB
